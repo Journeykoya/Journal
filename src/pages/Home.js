691a7555597769fbf1b6014dcc867aee110d6554
Kurdi <56204590+KurdCr@users.noreply.github.com>
2023-07-17 16:39:06 +0300

    .

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,10 +1,17 @@
+import Calender from "../Components/Calender";
+// import Calender2 from "../Components/Calender2";
+
+
 const Home = () => {
+
+
     return <>
     
     <h1 class="text-3xl text-red-200 font-bold underline">
     Hello world!
   </h1>
-    
+  <Calender/>
+  {/* <Calender2/> */}
     </>;
   };
   
