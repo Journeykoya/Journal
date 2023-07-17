599b999e65931c4f6c018b95909017f5c4f29e64
Kurdi <56204590+KurdCr@users.noreply.github.com>
2023-07-17 06:35:50 +0300

    .

--- src/index.js
+++ src/index.js
@@ -4,14 +4,34 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Layout from "./pages/Layout";
+import Home from "./pages/Home";
+import Contact from "./pages/Contact";
+import NotFound from "./pages/NotFound";
+
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+     <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path="contact" element={<Contact />} />
+          <Route path="*" element={<NotFound />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>
-);
+ );
+// root.render(<App />);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
+
+
+
---
+++ src/pages/Home.js
@@ -0,0 +1,11 @@
+const Home = () => {
+    return <>
+    
+    <h1 class="text-3xl text-red-200 font-bold underline">
+    Hello world!
+  </h1>
+    
+    </>;
+  };
+  
+  export default Home;
